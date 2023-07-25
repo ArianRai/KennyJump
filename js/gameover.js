46cf734b357ef363d36020278383c886813e34ea
H1V35 <higesjorge@gmail.com>
2023-07-25 16:17:42 +0200

    Hola

--- js/gameover.js
+++ js/gameover.js
@@ -1,23 +1,23 @@
-const gameOverScreen = document.createElement('div')
+const gameOverScreen = document.createElement("div")
 
-gameOverScreen.setAttribute('id', 'gameOver')
+gameOverScreen.setAttribute("id", "gameOver")
 gameOverScreen.style.width = `${window.innerHeight + 200}px`
 gameOverScreen.style.height = `${window.innerHeight}px`
-gameOverScreen.style.position = 'absolute'
-gameOverScreen.style.display = 'none'
+gameOverScreen.style.position = "absolute"
+// gameOverScreen.style.display = 'none'
 
-gameOverScreen.style.backgroundColor = 'black'
-gameOverScreen.style.zIndex = '400'
-gameOverScreen.style.marginLeft = '120px'
-let gameScreen = document.querySelector('#game-screen')
+gameOverScreen.style.backgroundColor = "black"
+gameOverScreen.style.zIndex = "400"
+gameOverScreen.style.marginLeft = "120px"
+let gameScreen = document.querySelector("#game-screen")
 document.body.insertBefore(gameOverScreen, gameScreen)
 
-const restartButton = document.createElement('button')
-restartButton.innerText = 'RESTART'
-restartButton.style.width = '300px'
-restartButton.style.height = '80px'
-restartButton.style.position = 'absolute'
-restartButton.style.bottom = '50px'
-restartButton.style.right = '100px'
+const restartButton = document.createElement("button")
+restartButton.innerText = "RESTART"
+restartButton.style.width = "300px"
+restartButton.style.height = "80px"
+restartButton.style.position = "absolute"
+restartButton.style.bottom = "50px"
+restartButton.style.right = "100px"
 
 gameOverScreen.appendChild(restartButton)
