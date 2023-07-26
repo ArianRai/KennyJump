428ac54c1f91d0cef566b0ccfd3bf08a3b5a37fc
Arian <arianraiesteban_20@hotmail.com>
2023-07-26 12:23:39 +0200

    estilos

--- js/startscreen.js
+++ js/startscreen.js
@@ -1,42 +1,45 @@
-const gameStartScreen = document.createElement("div")
+const gameStartScreen = document.createElement('div')
 
-gameStartScreen.setAttribute("id", "startscreen")
+gameStartScreen.setAttribute('id', 'startscreen')
 gameStartScreen.style.width = `${window.innerWidth}px`
 gameStartScreen.style.height = `${window.innerHeight}px`
-gameStartScreen.style.position = "absolute"
-gameStartScreen.style.display = "block"
-gameStartScreen.style.backgroundImage = "url(./img/KENNY_BG.png)"
-gameStartScreen.style.backgroundSize = "cover"
-gameStartScreen.style.zIndex = "400"
+gameStartScreen.style.position = 'absolute'
+gameStartScreen.style.display = 'block'
+gameStartScreen.style.backgroundImage = 'url(./img/KENNY_BG.png)'
+gameStartScreen.style.backgroundSize = 'cover'
+gameStartScreen.style.zIndex = '400'
 
 document.body.insertBefore(gameStartScreen, gameScreen)
 
-const nameInput = document.createElement("input")
+const nameInput = document.createElement('input')
 
-nameInput.style.width = "300px"
-nameInput.style.height = "60px"
-nameInput.style.position = "absolute"
+nameInput.style.width = '300px'
+nameInput.style.height = '60px'
+nameInput.style.position = 'absolute'
+nameInput.style.fontSize = '24px'
+nameInput.style.textAlign = 'center'
+
 nameInput.style.bottom = `${window.innerHeight * 0.3}px`
 nameInput.style.right = `${window.innerWidth * 0.5 - 150}px`
 
 gameStartScreen.appendChild(nameInput)
 
-const startGameButton = document.createElement("button")
+const startGameButton = document.createElement('button')
 
-startGameButton.style.width = "300px"
-startGameButton.style.height = "60px"
-startGameButton.style.position = "absolute"
-startGameButton.innerText = "START"
+startGameButton.style.width = '300px'
+startGameButton.style.height = '60px'
+startGameButton.style.position = 'absolute'
+startGameButton.innerText = 'START'
 startGameButton.style.bottom = `${window.innerHeight * 0.2}px`
 startGameButton.style.right = `${window.innerWidth * 0.5 - 150}px`
 
 gameStartScreen.appendChild(startGameButton)
 
-let playerName = "PLAYER"
+let playerName = 'PLAYER'
 
 startGameButton.onclick = () => {
-    gameStartScreen.style.display = "none"
-    if (nameInput.value) {
-        playerName = nameInput.value
-    }
+	gameStartScreen.style.display = 'none'
+	if (nameInput.value) {
+		playerName = nameInput.value
+	}
 }
